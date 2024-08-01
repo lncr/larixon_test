e2ba07a36fc2ba95c8203c5894f9b0aab69ee887
Bektur Soltobaev <bektur_soltobaev@epam.com>
2024-08-01 22:53:13 +0600

    Add models and migration files

--- adverts/models.py
+++ adverts/models.py
@@ -1,2 +1,19 @@
 from django.db import models
 
+
+class City(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Advert(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    views = models.PositiveIntegerField(default=0)
+
+    city = models.ForeignKey(City, on_delete=models.SET_NULL, related_name='adverts', null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, related_name='adverts', null=True)
